911b47cca8b95f883749d2ad4eeab6c088d51b00
m04sengy@gmail.com <m04sengy@gmail.com@d9c74e65-7642-4654-cbf7-8a486b1c7b57>
2013-02-14 08:59:42 +0000

    This is a HelloWorld program implemented with interfaces and test driven development

---
+++ HelloWorld/src/com/azariah/hello/HelloWorld.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.azariah.hello;
+
+/**
+ *
+ * @author 210192461
+ */
+public interface HelloWorld {
+
+    String getString();
+    
+}
---
+++ HelloWorld/src/com/azariah/hello/HelloWorldImpl.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.azariah.hello;
+
+/**
+ *
+ * @author 210192461
+ */
+public class HelloWorldImpl implements HelloWorld {
+
+    public HelloWorldImpl() {
+    }
+
+    @Override
+    public String getString() {
+        return "Hello World";
+    }
+    
+    
+}
