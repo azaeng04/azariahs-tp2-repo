fbfaa95205b8f118a5f13a675a184f547e428041
m04sengy@gmail.com <m04sengy@gmail.com@d9c74e65-7642-4654-cbf7-8a486b1c7b57>
2013-04-22 10:49:01 +0000

    Domain model fixed

--- LiveProductFeedSystem/src/java/com/store/feed/domain/Customer.java
+++ LiveProductFeedSystem/src/java/com/store/feed/domain/Customer.java
@@ -5,27 +5,45 @@
 package com.store.feed.domain;
 
 import java.io.Serializable;
-import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
 
 /**
  *
  * @author Ronald
  */
 @Entity
-public class Customer extends Person implements Serializable {
+public class Customer implements Person, Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String customerNumber;
-        
+    private String firstName;
+    private String lastName;
+    private String middleName;
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date dateOfBirth;
+    private String gender;
+
+    @OneToMany
+    private List<Address> addresses;
+    
+    @Embedded
+    private Contact contact;
+    
+    @OneToOne
+    private Users user;
+    
     @OneToMany
     @JoinColumn(name = "customerNumber")
     private List<Notification> notifications;
@@ -78,5 +96,84 @@ public class Customer extends Person implements Serializable {
     public String toString() {
         return "com.store.feed.domain.Customer[ id=" + id + " ]";
     }
-    
+
+    @Override
+    public String getFirstName() {
+        return firstName;
+    }
+
+    @Override
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Override
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    @Override
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    @Override
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    @Override
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    @Override
+    public String getGender() {
+        return gender;
+    }
+
+    @Override
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public List<Address> getAddresses() {
+        return addresses;
+    }
+
+    @Override
+    public void setAddresses(List<Address> addresses) {
+        this.addresses = addresses;
+    }
+
+    @Override
+    public Contact getContact() {
+        return contact;
+    }
+
+    @Override
+    public void setContact(Contact contact) {
+        this.contact = contact;
+    }
+
+    @Override
+    public Users getUser() {
+        return user;
+    }
+
+    @Override
+    public void setUser(Users user) {
+        this.user = user;
+    }
 }
