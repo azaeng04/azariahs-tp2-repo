3db04a5bfa170d0aa53a8cd912589bc612a414dc
m04sengy <m04sengy@gmail.com>
2013-04-22 10:49:01 +0000

    Domain model fixed

--- LiveProductFeedSystem/src/java/com/store/feed/domain/Person.java
+++ LiveProductFeedSystem/src/java/com/store/feed/domain/Person.java
@@ -4,74 +4,44 @@
  */
 package com.store.feed.domain;
 
-import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
-import javax.persistence.Embedded;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 
 /**
  *
  * @author Ronald
  */
-public abstract class Person implements Serializable {
-    private String firstName;
-    private String lastName;
-    private String middleName;
-    private Date dateOfBirth;
-    private String gender;
+public interface Person {
 
-    @OneToMany
-    private List<Address> addresses;
-    
-    @Embedded
-    private Contact contact;
-    
-    @OneToOne
-    private Users user;
+    public List<Address> getAddresses();
 
-    public List<Address> getAddresses() {
-        return addresses;
-    }
+    public void setAddresses(List<Address> addresses);
 
-    public void setAddresses(List<Address> addresses) {
-        this.addresses = addresses;
-    }
+    public Contact getContact();
 
-    public Contact getContact() {
-        return contact;
-    }
+    public void setContact(Contact contact);
 
-    public void setContact(Contact contact) {
-        this.contact = contact;
-    }
+    public Users getUser();
 
-    public Users getUser() {
-        return user;
-    }
+    public void setUser(Users user);
 
-    public void setUser(Users user) {
-        this.user = user;
-    }
-    
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+    public void setFirstName(String firstName);
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+    public String getFirstName();
 
-    public void setMiddleName(String middleName) {
-        this.middleName = middleName;
-    }
+    public void setLastName(String lastName);
 
-    public void setDateOfBirth(Date dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
+    public String getLastName();
 
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
+    public void setMiddleName(String middleName);
+
+    public String getMiddleName();
+
+    public void setDateOfBirth(Date dateOfBirth);
+
+    public Date getDateOfBirth();
+
+    public void setGender(String gender);
+
+    public String getGender();
 }
