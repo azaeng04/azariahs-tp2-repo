8e9b4bcc4a71084a84406606f15ecebb67c56a49
m04sengy <m04sengy@gmail.com>
2013-04-25 21:31:26 +0000

    Implementation of the Builder Pattern

---
+++ src/java/com/design/pattern/builder/service/MacMealService.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.design.pattern.builder.service;
+
+import com.design.pattern.builder.model.MacMeal;
+
+/**
+ *
+ * @author 210192461
+ */
+public interface MacMealService {
+
+    public MacMeal placeOrder(MacMeal order);
+}
