29cc1963a9e398605ee4bcdbe721538f2c19bb44
m04sengy <m04sengy@gmail.com>
2013-05-08 07:48:46 +0000

    Working on login form

---
+++ LiveProductFeedSystem/src/java/com/store/feed/client/web/jsf/controller/LoginController.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.store.feed.client.web.jsf.controller;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author 210192461
+ */
+@ManagedBean
+@SessionScoped
+public class LoginController {
+    public void checkLoginDetails() {
+        
+    }
+
+}
